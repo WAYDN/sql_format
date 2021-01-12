54c6e4e24a35763581d59fe273af6a6c899ee335
waydn <ernestwq@163.com>
2021-01-12 21:34:34 +0800

    20210112 wq 1.增加对建表语句的格式化(3.3.0)

--- sql_format_exec.py
+++ sql_format_exec.py
@@ -29,16 +29,24 @@ def sql_split(sql, is_comma_trans=False, space_num=2):
     """
     # 分割sql, 结尾加\s 防止将非关键字给分割了 例如pdw_fact_person_insure中的on
     # 20190326 wq 在关键字前后增加\s，防止将非关键字给分割了，例如sql_from中的from
-
-    split_sql = re.findall(r'(((^(\s*--\s*[^\s]*)+|with.*?\(|\w+\sas\s*\()|'
-                           r'(select|from|((left|right|full|inner|cross)\s(outer\s)?)?join|'
-                           r'on|where|group|order|limit|having|union(\sall)?|insert|create|lateral\sview))'
-                           r'.*?'
-                           r'\s(?=(with.*?\(|\w+\sas\s*\()|'
-                           r'(select|from|((left|right|full|inner|cross)\s(outer\s)?)?join\(?|'
-                           r'on|where|group|order|limit|having|union(\sall)?|insert|create|lateral\sview)\s|$))', sql)
+    if re.search(r'create.*(?!=select)', sql):
+        split_sql = re.findall(r'((create|partitioned|clustered|sorted by|stored as|into|row format|location)'
+                               r'.*?'
+                               r'\s(?=(create|partitioned|clustered|sorted by|stored as|into|row format|location)|$))',
+                               sql)
+        is_create = 1
+    else:
+        split_sql = re.findall(r'(((^(\s*--\s*[^\s]*)+|with.*?\(|\w+\sas\s*\()|'
+                               r'(select|from|((left|right|full|inner|cross)\s(outer\s)?)?join|'
+                               r'on|where|group|order|limit|having|union(\sall)?|insert|create|lateral\sview))'
+                               r'.*?'
+                               r'\s(?=(with.*?\(|\w+\sas\s*\()|'
+                               r'(select|from|((left|right|full|inner|cross)\s(outer\s)?)?join\(?|'
+                               r'on|where|group|order|limit|having|union(\sall)?|insert|create|lateral\sview)\s|$))',
+                               sql)
     split_sql_list = [split_sql_value[0].lstrip() for split_sql_value in split_sql]
     # 20190319 wq 消除窗口函数中order等字段中含关键字的影响,将select到from或select到union或select整合在一起
+    # 根据括号合并被分割的函数
     split_sql_list_pos = 0
     while split_sql_list_pos < len(split_sql_list)-1 and len(split_sql_list) > 1:
         if not re.search('^(from|select|union) ', split_sql_list[split_sql_list_pos+1])\
@@ -64,29 +72,45 @@ def sql_split(sql, is_comma_trans=False, space_num=2):
         for exec_sql_pos in range(len(exec_sql)):
             exec_sql_value = exec_sql[exec_sql_pos].strip()
             first_value = re.match(r'\s*(--|\w+|\))\s?', exec_sql_value).group(1)
-            if first_value in ('select', 'group', 'order'):
+            if first_value in ('select', 'group', 'order') or re.search(r'^create.*(?!=select)', exec_sql_value):
                 # 分割字段，根据','分割出所有字段
                 tmp_sql = [i[0].strip() for i in re.findall(r'(.*?(,(\s*--\s*[^\s]*)?|$))', exec_sql_value)
                            if i[0].strip() != '']
                 tmp = []
-                # 20190319 wq 合并函数中的括号和中括号
-                for tmp_sql_pos in range(len(tmp_sql)):
-                    if tmp_sql_pos > 0 and (tmp_sql[tmp_sql_pos-1].count('(') != tmp_sql[tmp_sql_pos-1].count(')')
-                                            or tmp_sql[tmp_sql_pos-1].count('[') != tmp_sql[tmp_sql_pos-1].count(']')):
-                        tmp_sql[tmp_sql_pos] = tmp_sql[tmp_sql_pos-1] + ' ' + tmp_sql[tmp_sql_pos]
-                        # 跳过上次括号不齐，留到下次合并处理
-                        if tmp_sql[tmp_sql_pos].count('(') == tmp_sql[tmp_sql_pos].count(')') \
+                if first_value in ('select', 'group', 'order'):
+                    # 20190319 wq 合并函数中的括号和中括号
+                    for tmp_sql_pos in range(len(tmp_sql)):
+                        if tmp_sql_pos > 0 \
+                                and (tmp_sql[tmp_sql_pos - 1].count('(') != tmp_sql[tmp_sql_pos - 1].count(')')
+                                     or tmp_sql[tmp_sql_pos - 1].count('[') != tmp_sql[tmp_sql_pos - 1].count(']')):
+                            tmp_sql[tmp_sql_pos] = tmp_sql[tmp_sql_pos - 1] + ' ' + tmp_sql[tmp_sql_pos]
+                            # 跳过上次括号不齐，留到下次合并处理
+                            if tmp_sql[tmp_sql_pos].count('(') == tmp_sql[tmp_sql_pos].count(')') \
+                                    and tmp_sql[tmp_sql_pos].count('[') == tmp_sql[tmp_sql_pos].count(']'):
+                                tmp.append(tmp_sql[tmp_sql_pos])
+                        elif tmp_sql[tmp_sql_pos].count('(') == tmp_sql[tmp_sql_pos].count(')') \
                                 and tmp_sql[tmp_sql_pos].count('[') == tmp_sql[tmp_sql_pos].count(']'):
                             tmp.append(tmp_sql[tmp_sql_pos])
-                    elif tmp_sql[tmp_sql_pos].count('(') == tmp_sql[tmp_sql_pos].count(')') \
-                            and tmp_sql[tmp_sql_pos].count('[') == tmp_sql[tmp_sql_pos].count(']'):
-                        tmp.append(tmp_sql[tmp_sql_pos])
-                    else:
-                        pass
+                        else:
+                            pass
+                else:
+                    tmp_sql = list_remake([re.findall(r'([^(]+\(|(?<=\()[^(]+|[^)]+(?=\))|\)[^)]+)', i)
+                                           if re.search('\(|\)', i) else i for i in tmp_sql])
+                    # 20210112 wq 合并<>
+                    for tmp_sql_pos in range(len(tmp_sql)):
+                        if tmp_sql_pos > 0 and tmp_sql[tmp_sql_pos - 1].count('<') != tmp_sql[tmp_sql_pos - 1].count(
+                                '>'):
+                            tmp_sql[tmp_sql_pos] = tmp_sql[tmp_sql_pos - 1] + ' ' + tmp_sql[tmp_sql_pos]
+                            if tmp_sql[tmp_sql_pos].count('<') == tmp_sql[tmp_sql_pos].count('>'):
+                                tmp.append(tmp_sql[tmp_sql_pos].strip())
+                        elif tmp_sql[tmp_sql_pos].count('<') == tmp_sql[tmp_sql_pos].count('>'):
+                            tmp.append(tmp_sql[tmp_sql_pos].strip())
+                        else:
+                            pass
                 # 添加字段前空格
                 for tmp_pos in range(len(tmp)):
                     # 20190321 wq 修复逗号前置和字段中含注释所导致的错误（逗号被注释掉）
-                    if re.search(r'[^,]+--z\d+s,$', tmp[tmp_pos]) is not None:
+                    if re.search(r'[^,]+--z\d+s,$', tmp[tmp_pos]):
                         tmp[tmp_pos] = re.sub(',$', '', re.sub(r'(?<=[^,])\s*--', ', --', tmp[tmp_pos], 1))
                     if tmp_pos == 0:
                         if is_comma_trans is True:
@@ -154,6 +178,8 @@ def sql_split(sql, is_comma_trans=False, space_num=2):
                 if first_value != ')':
                     if re.search(r'^\w+ as \((\s*--.*)?$', exec_sql[exec_sql_pos]):
                         exec_sql[exec_sql_pos] = (6 + space_num) * " " + exec_sql[exec_sql_pos]
+                    elif len(first_value) > 6:
+                        pass
                     else:
                         exec_sql[exec_sql_pos] = re.sub(r'^\s*(--|\w*)\s*', first_value.rjust(6) + space_num * " ",
                                                         re.sub(r'\s*\(', ' (', exec_sql[exec_sql_pos]))
@@ -190,33 +216,34 @@ def sql_format(sql, is_comma_trans=False, space_num=2):
     # 统一空白符
     sql = ' ' + re.sub(r'\s+', ' ', sql).strip() + ' '
     tmp_sql = sql.lower()
-    for pattern_atom in [r'\[', r'\(', r'\]', ',', r'\)', r'\+', '-', r'\*', '/', '=', '<', '>', '!']:
-        pattern = '[ ]*{0}[ ]*'.format(pattern_atom)
-        if pattern_atom in (r'\[', r'\('):
-            repl_str = re.sub(r'\\', '', pattern_atom)
-            pattern = r'(?<=(\w|\(|\[|\)|\]))' + pattern
-        elif pattern_atom in [r'\]', r'\)', r',']:
-            repl_str = re.sub(r'\\', '', pattern_atom) + ' '
-            pattern = r'(?<=(\w|\(|\[|\)|\]))' + pattern
-        elif pattern_atom in [r'\+', r'\*', '/', '=', '<', '>', '!']:
-            repl_str = ' ' + re.sub(r'\\', '', pattern_atom) + ' '
-        elif pattern_atom == '-':
-            pattern = '[ ]*(?<!-)-(?!-)[ ]*'
-            repl_str = ' - '
-        else:
-            pass
-        tmp_sql = re.sub(pattern, repl_str, tmp_sql)
-    # 20190321 wq 优化符号的处理
-    tmp_sql = re.sub(r'(?<=\[|\]|,|\(|\))\s*(?=\[|\]|,|\(|\))', '', tmp_sql)
-    tmp_sql = re.sub(r'(?<=!|=|<|>)\s*(?=!|=|<|>)', '', tmp_sql)
-    # 字段中符号开头，例如"100 * -1"
-    tmp_sql = re.sub(r'(?<=\D\s(\+|-))\s*', '', tmp_sql)
+    if not re.search(r'^(\W*)create ', tmp_sql):
+        for pattern_atom in [r'\[', r'\(', r'\]', ',', r'\)', r'\+', '-', r'\*', '/', '=', '<', '>', '!']:
+            pattern = '[ ]*{0}[ ]*'.format(pattern_atom)
+            if pattern_atom in (r'\[', r'\('):
+                rep_str = re.sub(r'\\', '', pattern_atom)
+                pattern = r'(?<=(\w|\(|\[|\)|\]))' + pattern
+            elif pattern_atom in [r'\]', r'\)', r',']:
+                rep_str = re.sub(r'\\', '', pattern_atom) + ' '
+                pattern = r'(?<=(\w|\(|\[|\)|\]))' + pattern
+            elif pattern_atom in [r'\+', r'\*', '/', '=', '<', '>', '!']:
+                rep_str = ' ' + re.sub(r'\\', '', pattern_atom) + ' '
+            elif pattern_atom == '-':
+                pattern = '[ ]*(?<!-)-(?!-)[ ]*'
+                rep_str = ' - '
+            else:
+                rep_str = ''
+            tmp_sql = re.sub(pattern, rep_str, tmp_sql)
+        # 20190321 wq 优化符号的处理
+        tmp_sql = re.sub(r'(?<=[\[\],()])\s*(?=[\[\],()])', '', tmp_sql)
+        tmp_sql = re.sub(r'(?<=[!=<>])\s*(?=[!=<>])', '', tmp_sql)
+        # 字段中符号开头，例如"100 * -1"
+        tmp_sql = re.sub(r'(?<=\D\s[+-])\s*', '', tmp_sql)
     # 20190326 wq 修复子查询的问题
     tmp_sql = tmp_sql.replace('(--', '( --').replace('(select ', '( select ')
     # 20190312 wq 关键字后直接接左括号
     tmp_sql = re.sub(r'((?<=\Wselect)|(?<=\Wfrom)|(?<=\Wjoin)|(?<=\Won)|(?<=\Wover)|(?<=\Wand)|(?<=\Wor)|'
                      r'(?<=\Wwhere)|(?<=\Wby)|(?<=\Whaving)|(?<=\Was)|(?<=\Win))\(', ' (', tmp_sql)
-    # 按括号添加前缀空格
+
     table_list = []
     custom_table_list = []
     for tmp_sql_value in tmp_sql.split(';'):
@@ -226,7 +253,7 @@ def sql_format(sql, is_comma_trans=False, space_num=2):
             # 20190328 wq 获取from后的表，再与with/as的自定义表名对比，剔除
             # 20190402 wq 1.获取表名：增加join的判断
             for split_sql_value in split_sql:
-                if re.match(r'^\s*(from|((left|right|full|inner|cross)\s+(outer\s+)?)?join)\s+[^\(]+$',
+                if re.match(r'^\s*(from|((left|right|full|inner|cross)\s+(outer\s+)?)?join)\s+[^(]+$',
                             split_sql_value):
                     table_list.append(re.search(r'(from|join)\s+(.+?)(?=--|\s|$)', split_sql_value).group(2))
                 elif re.match(r'^\s*\swith.*?\(|[^,]*as\s*\(', split_sql_value):
@@ -237,7 +264,10 @@ def sql_format(sql, is_comma_trans=False, space_num=2):
                 if re.match(r'^\s*$', split_sql_value):
                     continue
                 tmp_result_sql = tmp_result_sql + level * 8 * " " + split_sql_value + "\r\n"
-                if re.search(r'\((\s*--\s*[^\s]*)?\s*$', split_sql_value):
+                # 按括号添加前缀空格，create跳过添加前缀空格
+                if re.search(r'create.*(?!=select)', tmp_sql_value):
+                    pass
+                elif re.search(r'\((\s*--\s*[^\s]*)?\s*$', split_sql_value):
                     level += 1
                 elif re.search(r'^\s*\)\s*.*?,?\s*$', split_sql_value):
                     level -= 1
@@ -273,19 +303,32 @@ def sql_format(sql, is_comma_trans=False, space_num=2):
 
 
 if __name__ == '__main__':
-    exec_sql = [
+    original_sql = [
         """
         with asss as (
         select 123),
         dds as ( --fesfa
         select -123),da as (select 321) --123
-select 1+1, -1-2, 1*-2 , +   +1 regexp_extract(map_col, 'from_sign_id":"([^2]+)"', 1), case when coalesce(from_object, '') not in ('icon', '') then -12+-25 else -123*-2 end as from_content, count(1) from pdw.fact_stock_em_web_log 
-where hp_stat_date between '2019-11-01' and '2019-11-28' and objects rlike '^(ssbb|neican)_\\d+$' and user_id <> 0 group by 1
-        """
+select 1+1, -1-2, 1*-2 , +   +1 regexp_extract(map_col, 'from_sign_id":"([^2]+)"', 1) [1], case when 
+coalesce(from_object, '') not in ('icon', '') then -12+-25 else -123*-2 end as from_content, count(1) 
+from pdw.fact_stock_em_web_log 
+where hp_stat_date between '2019-11-01' and '2019-11-28' and objects rlike '^(ssbb|neican)_\\d+$' and user_id <> 0 
+group by 1
+        """,
+        """   CREATE EXTERNAL TABLE cdm.t_device_user_bind_tags (
+guid string COMMENT '设备id',
+user_id string  COMMENT '账号id',
+tag string  COMMENT '标签',
+is_new int comment '是否最新数据',
+create_time string,
+update_time string
+) comment '设备用户绑定标签表'
+ partitioned by (dt string  comment '分区字段')
+  row format delimited fields terminated by '\t' stored as PARQUET TBLPROPERTIES('parquet.compression'='SNAPPY')""",
+        """create table db_test.wq_test as select 123 as ddd, 321 as ds"""
     ]
-    for exec_sql_vaule in exec_sql:
+    for exec_sql_vaule in [original_sql[1]]:
         # print(exec_sql_vaule)
         # print("------------------------无情分割线-----------------------")
-        format_sql = sql_format(exec_sql_vaule, False, 1)
+        format_sql = sql_format(exec_sql_vaule, False, 2)
         print(format_sql[0])
-        print(format_sql[1])
